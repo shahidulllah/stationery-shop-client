a5452adec1ecb5edee9c829c162bab4b89beaec0
shahidulllah <mdshahidsumon177@gmail.com>
2025-01-27 23:21:09 +0600

    project setup with nav design

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import MainLayout from "./layout/MainLayout";
+
+export default function App() {
+  return (
+    <h1 className="text-6xl underline">
+      <MainLayout/>
+    </h1>
+  )
+}
---
+++ src/layout/MainLayout.tsx
@@ -0,0 +1,12 @@
+import Navbar from "../pages/home/navbar/Navbar";
+
+
+const MainLayout = () => {
+    return (
+        <div>
+          <Navbar/>
+        </div>
+    );
+};
+
+export default MainLayout;
