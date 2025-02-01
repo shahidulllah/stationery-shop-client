f0463671d3b638475b8e66b28c7ab6147cfc1d4a
shahidulllah <mdshahidsumon177@gmail.com>
2025-02-01 13:03:42 +0600

    added auth api

---
+++ src/redux/api/authApi.ts
@@ -0,0 +1,29 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const BASE_URL = "http://localhost:3000/api";
+
+export const authApi = createApi({
+  reducerPath: "authApi",
+  baseQuery: fetchBaseQuery({ baseUrl: BASE_URL }),
+  endpoints: (builder) => ({
+    // register
+    registerUser: builder.mutation({
+      query: (userData) => ({
+        url: "/auth/register",
+        method: "POST",
+        body: userData,
+      }),
+    }),
+
+    //Login
+    loginUser: builder.mutation({
+      query: (credentials) => ({
+        url: "/auth/login",
+        method: "POST",
+        body: credentials,
+      }),
+    }),
+  }),
+});
+
+export const {useRegisterUserMutation, useLoginUserMutation} = authApi;
