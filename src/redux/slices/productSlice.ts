8189acdc841a2aaad97f690ab24943e6af6e0742
shahidulllah <mdshahidsumon177@gmail.com>
2025-02-02 12:21:07 +0600

    working on cart slice

---
+++ src/redux/slices/productSlice.ts
@@ -0,0 +1,46 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const fetchProducts = createAsyncThunk(
+  "products/fetchProducts",
+  async () => {
+    const response = await axios.get("/api/products");
+    return response.data;
+  }
+);
+
+// Define initial state
+interface ProductState {
+  products: any[];
+  status: "idle" | "loading" | "succeeded" | "failed";
+  error: string | null;
+}
+
+const initialState: ProductState = {
+  products: [],
+  status: "idle",
+  error: null,
+};
+
+const productSlice = createSlice({
+  name: "products",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchProducts.pending, (state) => {
+        state.status = "loading";
+      })
+      .addCase(fetchProducts.fulfilled, (state, action) => {
+        state.status = "succeeded";
+        state.products = action.payload;
+      })
+      .addCase(fetchProducts.rejected, (state, action) => {
+        state.status = "failed";
+        state.error = action.error.message ?? "An unknown error occurred";
+      });
+  },
+});
+
+export default productSlice.reducer;
