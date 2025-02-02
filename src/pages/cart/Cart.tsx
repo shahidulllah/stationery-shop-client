86aab6527de319281547406f04e79a9ab3b52f36
shahidulllah <mdshahidsumon177@gmail.com>
2025-02-02 23:21:05 +0600

    modify cartPage

--- src/pages/cart/Cart.tsx
+++ src/pages/cart/Cart.tsx
@@ -1,10 +1,15 @@
 import { useDispatch, useSelector } from "react-redux";
-import { RootState } from "@/redux/store";
-import { removeFromCart, clearCart } from "@/redux/slices/cartSlice";
+import { AppDispatch, RootState } from "@/redux/store";
+import { useEffect } from "react";
+import { clearCart, fetchCart, removeFromCart } from "@/redux/slices/cartSlice";
 
 const Cart = () => {
-  const dispatch = useDispatch();
+  const dispatch = useDispatch<AppDispatch>();
   const cartItems = useSelector((state: RootState) => state.cart.items);
+
+  useEffect(() => {
+    dispatch(fetchCart());
+  }, [dispatch]);
 
   if (cartItems.length === 0) return <p className="">Your cart is empty.</p>;
 
@@ -12,10 +17,12 @@ const Cart = () => {
     <div className="mt-24">
       <h2 className="text-2xl font-bold mb-4">Shopping Cart</h2>
       {cartItems.map((item) => (
-        <div key={item.id} className="flex justify-between p-2 border-b">
-          <span>{item.name} - ${item.price} x {item.quantity}</span>
+        <div key={item.product} className="flex justify-between p-2 border-b">
+          <span>
+            {item.product} - {item.quantity}
+          </span>
           <button
-            onClick={() => dispatch(removeFromCart(item.id))}
+            onClick={() => dispatch(removeFromCart(item.product))}
             className="text-red-500"
           >
             Remove
